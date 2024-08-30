08c82cadb5ca0f248b772eb6974fbe9081ac725b
Dmitry Rubinstein <rubinsteindb@gmail.com>
2024-08-30 20:15:05 +0300

    Initial commit

---
+++ webview-ui/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { provideVSCodeDesignSystem, allComponents } from "@vscode/webview-ui-toolkit";
+	import { vscode } from "./utilities/vscode";
+	
+	provideVSCodeDesignSystem().register(allComponents);
+
+	function handleHowdyClick() {
+		vscode.postMessage({
+			command: "hello",
+			text: "Hey there partner! 🤠",
+		});
+	}
+</script>
+
+<main>
+	<h1>Hello world!</h1>
+	<vscode-button on:click={handleHowdyClick}>Howdy!</vscode-button>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-start;
+		height: 100%;
+	}
+</style>
